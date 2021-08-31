7dfb72f8343f709a5fbf685a1f3d70b4ab6a9182
kristof22122 <kristf44@gmail.com>
2021-08-31 13:02:16 +0300

    Updates

---
+++ precache-manifest.762a15f023e24f63d8c6efada9c6b61a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4819fa70ae63ac29141689342529c7f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b094ebcc1f21d65c27e9",
+    "url": "./static/js/main.6fa6d798.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
